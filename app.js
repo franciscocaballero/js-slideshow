63864fff042e9d8ef2724de4489af85146b3b1f3
franciscocaballero <caballerofrancisco22@yahoo.com>
2017-07-14 21:00:39 -0400

    init slideshow set up

---
+++ app.js
@@ -0,0 +1,17 @@
+// start point
+var i = 0;
+//images arr
+images = [];
+// slide set to 2 seconds
+var time = 2000;
+
+//image list
+images[0] = '';
+images[1] = '';
+images[2] = '';
+
+//functon thatchanges images
+
+function changeImage(){
+  
+}
