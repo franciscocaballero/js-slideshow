0d0a9389b7c644289aa59157dbc996083f729d76
franciscocaballero <caballerofrancisco22@yahoo.com>
2017-07-14 21:22:29 -0400

    set up image  function

--- app.js
+++ app.js
@@ -10,8 +10,17 @@ images[0] = '';
 images[1] = '';
 images[2] = '';
 
-//functon thatchanges images
+//function that changes images
 
 function changeImage(){
-  
+
+  document.slide.src = images[i];
+
+   if(i < images.length -1 ; ){
+     i++;
+   } else {
+     i = 0;
+   }
+
+   setTimeout("changeImage()" time);
 }
